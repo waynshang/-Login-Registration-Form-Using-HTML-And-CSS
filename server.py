f1f7c4933eaa6a4035eabe631bc77149157170b1
wayne18308@gamil.com <wayne18308@gmail.com>
2020-07-19 13:29:12 +0800

    create home page

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'wayeshang'
 
-
 @app.route("/")
 def home():
   if "user_id" in session:
@@ -14,16 +13,19 @@ def home():
     return render_template("index.html", name = session['user_id'])
   else:
     return redirect("/login")
-  return render_template("login.html")
+  
 
 @app.route("/login", methods =["GET", "POST"])
 def login():
+  session.clear()
   if request.method == "POST":
+    
     user_id = request.form.get("user_id")
     user_password = request.form.get("user_password")
     is_user_valid = check_user_password(user_id, user_password)
     session["user_id"] = user_id if is_user_valid else session.clear()
     return redirect(url_for("home"))
+
   else:
     return render_template("login.html")
 
@@ -34,4 +36,3 @@ def register():
     session[key] = value
   # use url_for cause it can find the url via its function when route change can still find right url
   return redirect(url_for("home"))
-   
