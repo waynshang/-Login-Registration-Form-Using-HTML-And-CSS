a50e33a8bdd3d24c4cde85746ef522245d84b799
wayne18308@gamil.com <wayne18308@gmail.com>
2020-07-26 08:40:37 +0800

    add message add logout function add control session lifecycle

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from helper import check_user_password
+from datetime import timedelta
+
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'wayeshang'
-
+#session store in a specific folder in client server
+#if not assign session will be clear once browser close
+app.permanent_session_lifetime = timedelta(minute=5)
 
 @app.route("/")
 def home():
@@ -22,6 +25,8 @@ def login():
     user_id = request.form.get("user_id")
     user_password = request.form.get("user_password")
     is_user_valid = check_user_password(user_id, user_password)
+
+    session.permanent = True
     session["user_id"] = user_id if is_user_valid else session.clear()
     return redirect(url_for("home"))
   else:
@@ -37,8 +42,8 @@ def register():
 
 @app.route("/logout")
 def logout():
-  print(session)
   session.clear()
+  flash('You were successfully logged out')
   return redirect(url_for("login"))
 
 #----------------test
@@ -48,6 +53,13 @@ def test(name):
 @app.route("/test1")
 def test1():
   return redirect(url_for("test",name="wayne"))
+
+@app.route("/user")
+def user():
+  session["user"] = "wayne"
+  if "user" in session:
+    user = session["user"]
+    return redirect(url_for("test",name=user))
 if __name__ == "__main__":
   app.run(debug=True)
 
